13236c45a16c9ea632dd6d983dcc6a415a50b17b
Umesh <dev.codflaw@gmail.com>
2020-11-25 12:43:35 +0530

    admin file change

--- import_export/admin.py
+++ import_export/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Profile, Email_Dump
+from import_export.models import Profile, Email_Dump, DataSheetFromCommonNinja
 
 admin.site.register(Profile)
-admin.site.register(Email_Dump)
+# admin.site.register(Email_Dump)
+admin.site.register(DataSheetFromCommonNinja)
+
+
+@admin.register(Email_Dump)
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "email_confirmed")
